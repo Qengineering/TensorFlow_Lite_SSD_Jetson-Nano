fca1052ae9ad24bb3a9ab559fdf9b8fa43e01d6a
Q-engineering <info@qengineering.eu>
2021-02-02 14:49:05 +0100

    C++ source code

---
+++ MobileNetV1.cpp
@@ -0,0 +1,194 @@
+#include <stdio.h>
+#include <opencv2/opencv.hpp>
+#include <opencv2/dnn.hpp>
+#include <opencv2/highgui.hpp>
+#include <fstream>
+#include <iostream>
+#include <opencv2/core/ocl.hpp>
+#include "tensorflow/lite/interpreter.h"
+#include "tensorflow/lite/kernels/register.h"
+#include "tensorflow/lite/string_util.h"
+#include "tensorflow/lite/model.h"
+#include <cmath>
+
+/// Besides your regular TensorFlow Lite and flatbuffers library,
+/// you must also compiled TensorFlow Lite from scratch by bazel
+/// with the option GPU delegate set, before you can use the GPU delegates
+/// see https://qengineering.eu/install-tensorflow-2-lite-on-jetson-nano.html
+/// note also, it will not bring any speed improvement.
+//#define GPU_DELEGATE      //remove comment to deploy GPU delegates
+
+#ifdef GPU_DELEGATE
+    #include "tensorflow/lite/delegates/gpu/delegate.h"
+#endif // GPU_DELEGATE
+
+using namespace cv;
+using namespace std;
+
+int model_width;
+int model_height;
+int model_channels;
+
+std::vector<std::string> Labels;
+std::unique_ptr<tflite::Interpreter> interpreter;
+//-----------------------------------------------------------------------------------------------------------------------
+static bool getFileContent(std::string fileName)
+{
+
+	// Open the File
+	std::ifstream in(fileName.c_str());
+	// Check if object is valid
+	if(!in.is_open()) return false;
+
+	std::string str;
+	// Read the next line from File untill it reaches the end.
+	while (std::getline(in, str))
+	{
+		// Line contains string of length > 0 then save it in vector
+		if(str.size()>0) Labels.push_back(str);
+	}
+	// Close The File
+	in.close();
+	return true;
+}
+//-----------------------------------------------------------------------------------------------------------------------
+void detect_from_video(Mat &src)
+{
+    Mat image;
+    int cam_width =src.cols;
+    int cam_height=src.rows;
+
+    // copy image to input as input tensor
+    cv::resize(src, image, Size(model_width,model_height),INTER_NEAREST);
+    memcpy(interpreter->typed_input_tensor<uchar>(0), image.data, image.total() * image.elemSize());
+
+    interpreter->Invoke();      // run your model
+
+    const float* detection_locations = interpreter->tensor(interpreter->outputs()[0])->data.f;
+    const float* detection_classes=interpreter->tensor(interpreter->outputs()[1])->data.f;
+    const float* detection_scores = interpreter->tensor(interpreter->outputs()[2])->data.f;
+    const int    num_detections = *interpreter->tensor(interpreter->outputs()[3])->data.f;
+
+    //there are ALWAYS 10 detections no matter how many objects are detectable
+//        cout << "number of detections: " << num_detections << "\n";
+
+    const float confidence_threshold = 0.5;
+    for(int i = 0; i < num_detections; i++){
+        if(detection_scores[i] > confidence_threshold){
+            int  det_index = (int)detection_classes[i]+1;
+            float y1=detection_locations[4*i  ]*cam_height;
+            float x1=detection_locations[4*i+1]*cam_width;
+            float y2=detection_locations[4*i+2]*cam_height;
+            float x2=detection_locations[4*i+3]*cam_width;
+
+            Rect rec((int)x1, (int)y1, (int)(x2 - x1), (int)(y2 - y1));
+            rectangle(src,rec, Scalar(0, 0, 255), 1, 8, 0);
+            putText(src, format("%s", Labels[det_index].c_str()), Point(x1, y1-5) ,FONT_HERSHEY_SIMPLEX,0.5, Scalar(0, 0, 255), 1, 8, 0);
+        }
+    }
+}
+//-----------------------------------------------------------------------------------------------------------------------
+int main(int argc,char ** argv)
+{
+    float f;
+    float FPS[16];
+    int i, Fcnt=0;
+    Mat frame;
+    chrono::steady_clock::time_point Tbegin, Tend;
+
+    for(i=0;i<16;i++) FPS[i]=0.0;
+
+    // Load model
+    std::unique_ptr<tflite::FlatBufferModel> model = tflite::FlatBufferModel::BuildFromFile("detect.tflite");
+
+    // Build the interpreter
+    tflite::ops::builtin::BuiltinOpResolver resolver;
+    tflite::InterpreterBuilder(*model.get(), resolver)(&interpreter);
+
+#ifdef GPU_DELEGATE
+    TfLiteDelegate *MyDelegate = NULL;
+
+    const TfLiteGpuDelegateOptionsV2 options = {
+        .is_precision_loss_allowed = 1, //FP16,
+        .inference_preference = TFLITE_GPU_INFERENCE_PREFERENCE_FAST_SINGLE_ANSWER,
+        .inference_priority1 = TFLITE_GPU_INFERENCE_PRIORITY_MIN_LATENCY,
+        .inference_priority2 = TFLITE_GPU_INFERENCE_PRIORITY_AUTO,
+        .inference_priority3 = TFLITE_GPU_INFERENCE_PRIORITY_AUTO,
+    };
+    MyDelegate = TfLiteGpuDelegateV2Create(&options);
+
+    if(interpreter->ModifyGraphWithDelegate(MyDelegate) != kTfLiteOk) {
+        cerr << "ERROR: Unable to use delegate" << endl;
+        return 0;
+    }
+#endif // GPU_DELEGATE
+
+    interpreter->AllocateTensors();
+    interpreter->SetAllowFp16PrecisionForFp32(true);
+    interpreter->SetNumThreads(4);      //quad core
+
+    // Get input dimension from the input tensor metadata
+    // Assuming one input only
+    int In = interpreter->inputs()[0];
+    model_height   = interpreter->tensor(In)->dims->data[1];
+    model_width    = interpreter->tensor(In)->dims->data[2];
+    model_channels = interpreter->tensor(In)->dims->data[3];
+    cout << "height   : "<< model_height << endl;
+    cout << "width    : "<< model_width << endl;
+    cout << "channels : "<< model_channels << endl;
+
+	// Get the names
+	bool result = getFileContent("COCO_labels.txt");
+	if(!result)
+	{
+        cout << "loading labels failed";
+        exit(-1);
+	}
+
+    VideoCapture cap("James.mp4");
+    if (!cap.isOpened()) {
+        cerr << "ERROR: Unable to open the camera" << endl;
+        return 0;
+    }
+
+    cout << "Start grabbing, press ESC on Live window to terminate" << endl;
+	while(1){
+//        frame=imread("Traffic.jpg");  //need to refresh frame before dnn class detection
+        cap >> frame;
+        if (frame.empty()) {
+            cerr << "ERROR: Unable to grab from the camera" << endl;
+            break;
+        }
+
+        Tbegin = chrono::steady_clock::now();
+
+        detect_from_video(frame);
+
+        Tend = chrono::steady_clock::now();
+        //calculate frame rate
+        f = chrono::duration_cast <chrono::milliseconds> (Tend - Tbegin).count();
+        if(f>0.0) FPS[((Fcnt++)&0x0F)]=1000.0/f;
+        for(f=0.0, i=0;i<16;i++){ f+=FPS[i]; }
+        putText(frame, format("FPS %0.2f", f/16),Point(10,20),FONT_HERSHEY_SIMPLEX,0.6, Scalar(0, 0, 255));
+
+        //show output
+        imshow("Jetson Nano", frame);
+
+        char esc = waitKey(5);
+        if(esc == 27) break;
+    }
+
+    cout << "Closing the camera" << endl;
+    destroyAllWindows();
+
+#ifdef GPU_DELEGATE
+    interpreter.reset();
+    TfLiteGpuDelegateV2Delete(MyDelegate);
+#endif // GPU_DELEGATE
+
+    cout << "Bye!" << endl;
+
+  return 0;
+}
+//-----------------------------------------------------------------------------------------------------------------------
+
